0ea0d6bb06474746fcd1b98d2a966f0bcefc21ea
Ardyn Sulaeman <lemanfoetra@gmail.com>
2023-09-14 20:45:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h2>My Freinds</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+      <friend-contactxx></friend-contactxx>
+      <friend-contactxx></friend-contactxx>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 1,
+          name: "Manuel Lorenz",
+          phone: "0857937468",
+          email: "manuesl@mail.com",
+        },
+        {
+          id: 2,
+          name: "Manuel Lorenz 2",
+          phone: "0857937468",
+          email: "manuesl@mail.com",
+        },
+        {
+          id: 2,
+          name: "Manuel Lorenz 3",
+          phone: "0857937468",
+          email: "manuesl@mail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailAreVisible">
+      <li><strong>Phone : </strong> {{ friend.phone.phone }}</li>
+      <li><strong>Email : </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailAreVisible: false,
+      friend: {
+        id: 1,
+        name: "Manuel Lorenz",
+        phone: "0857937468",
+        email: "manuesl@mail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailAreVisible = !this.detailAreVisible;
+    },
+  },
+};
+</script>
